2bf8bb3d3f5491c5a1a904f638c0c4d8c36e4aea
Purujit Pradhan <purujit26@gmail.com>
2023-05-15 19:07:34 +0530

    ho gaya

--- index.js
+++ index.js
@@ -1,20 +1,208 @@
 const express = require('express');
-const foodData = require('./foodData.json');
 const app = express();
+const port = 3000;
 
-app.get('/health-api', function (req, res) { 
-  // You can include health API logic here
-})
+const foodData = 
+[
+  {
+    "id": 1,
+    "foodname": "Apple",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.3,
+    "cab": 25
+  },
+  {
+    "id": 2,
+    "foodname": "Banana",
+    "calorie": 105,
+    "category": "Fruit",
+    "protiens": 1.3,
+    "cab": 27
+  },
+  {
+    "id": 3,
+    "foodname": "Orange",
+    "calorie": 62,
+    "category": "Fruit",
+    "protiens": 1.2,
+    "cab": 15
+  },
+  {
+    "id": 4,
+    "foodname": "Pear",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.5,
+    "cab": 25
+  },
+  {
+    "id": 5,
+    "foodname": "Grapefruit",
+    "calorie": 42,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 11
+  },
+  {
+    "id": 6,
+    "foodname": "Strawberry",
+    "calorie": 46,
+    "category": "Fruit",
+    "protiens": 1.5,
+    "cab": 10
+  },
+  {
+    "id": 7,
+    "foodname": "Blueberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 14
+  },
+  {
+    "id": 8,
+    "foodname": "Raspberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.4,
+    "cab": 15
+  },
+  {
+    "id": 9,
+    "foodname": "Broccoli",
+    "calorie": 34,
+    "category": "Vegetable",
+    "protiens": 3.3,
+    "cab": 5
+  },
+  {
+    "id": 10,
+    "foodname": "Cauliflower",
+    "calorie": 25,
+    "category": "Vegetable",
+    "protiens": 2.6,
+    "cab": 5
+  },
+  {
+    "id": 11,
+    "foodname": "Green Beans",
+    "calorie": 31,
+    "category": "Vegetable",
+    "protiens": 2.4,
+    "cab": 4
+  },
+  {
+    "id": 12,
+    "foodname": "Asparagus",
+    "calorie": 20,
+    "category": "Vegetable",
+    "protiens": 2.2,
+    "cab": 2
+  },
+  {
+    "id": 13,
+    "foodname": "Spinach",
+    "calorie": 23,
+    "category": "Vegetable",
+    "protiens": 3.5,
+    "cab": 4
+  },
+  {
+    "id": 14,
+    "foodname": "Kale",
+    "calorie": 33,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 7
+  },
+  {
+    "id": 15,
+    "foodname": "Sweet Potato",
+    "calorie": 103,
+    "category": "Vegetable",
+    "protiens": 2,
+    "cab": 27
+  },
+  {
+    "id": 16,
+    "foodname": "Potato",
+    "calorie": 161,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 37
+  },
+  {"id": 17,
+    "foodname": "Carrot",
+    "calorie": 41,
+    "category": "Vegetable",
+    "protiens": 0.9,
+    "cab": 9
+  },
+  {
+    "id": 18,
+    "foodname": "Onion",
+    "calorie": 40,
+    "category": "Vegetable",
+    "protiens": 1.4,
+    "cab": 9
+  },
+  {
+    "id": 19,
+    "foodname": "Egg",
+    "calorie": 77,
+    "category": "Protein",
+    "protiens": 6.3,
+    "cab": 0.5
+  },
+  {
+    "id": 20,
+    "foodname": "Chicken Breast",
+    "calorie": 165,
+    "category": "Protein",
+    "protiens": 31,
+    "cab": 0
+  }
+]
+
+app.get('/health-api', (req, res) => {
+  const currentTime = new Date();
+  res.send({
+    time: currentTime,
+    app: 'express-server',
+    status: 'active'
+  });
+});
+
+app.get('/all', (req, res) => {
+  res.send(foodData);
+});
+
+app.get('/vegetable', (req, res) => {
+  const vegetableFoods = foodData.filter(food => food.category === 'Vegetable');
+  res.send(vegetableFoods);
+});
+
+app.get('/fruit', (req, res) => {
+  const fruitFoods = foodData.filter(food => food.category === 'Fruit');
+  res.send(fruitFoods);
+});
 
 app.get('/protein', (req, res) => {
-  const proteinFoods = foodData.foods.filter(food => food.category === 'protein');
+  const proteinFoods = foodData.filter(food => food.category === 'Protein');
   res.send(proteinFoods);
-})
+});
 
-app.get('/all', function (req, res) {
-  res.send(foodData);
-})
+app.get('/calorie-above-100', (req, res) => {
+  const calorieAbove100Foods = foodData.filter(food => food.calorie > 100);
+  res.send(calorieAbove100Foods);
+});
 
-app.listen(3001, () => {
-  console.log('Server listening on port 3001');
+app.get('/calorie-below-100', (req, res) => {
+  const calorieBelow100Foods = foodData.filter(food => food.calorie < 100);
+  res.send(calorieBelow100Foods);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
